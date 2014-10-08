f31674e8deb79e3bae9e11a3a568edf1059356be
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2014-10-07 23:52:49 -0400

    Criando classes de validação para os models Perfil e Permissao; Removendo caractere da view Perfil/Novo.

---
+++ Autoriza/Autoriza/Models/Validation/PerfilValidation.cs
@@ -0,0 +1,26 @@
+﻿using Autoriza.DAO;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Autoriza.Models.Validation
+{
+    public class PerfilValidation : AbstractValidator<Perfil>
+    {
+
+        private PerfilDAO PerfilDAO;
+
+        public PerfilValidation(PerfilDAO perfilDAO)
+        {
+            PerfilDAO = perfilDAO;
+
+            RuleFor(s => s.Nome).NotEmpty();
+            RuleFor(s => s.Descricao).NotEmpty();
+        }
+
+        // TODO Verificar se já existe um perfil com o mesmo nome para o sistema.
+
+    }
+}
---
+++ Autoriza/Autoriza/Models/Validation/PermissaoValidation.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Autoriza.Models.Validation
+{
+    public class PermissaoValidation : AbstractValidator<Permissao>
+    {
+        public PermissaoValidation()
+        {
+            RuleFor(s => s.Nome).NotEmpty();
+            RuleFor(s => s.Descricao).NotEmpty();
+        }
+
+        // TODO Verificar se já existe uma permissão com o mesmo nome para o sistema.
+
+    }
+}
