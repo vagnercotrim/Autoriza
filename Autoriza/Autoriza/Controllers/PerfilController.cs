35d2c0056a8fddbb2c6729df0f77dcfa2c421c86
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2014-10-07 23:40:00 -0400

    Funcionalidade: editar dados de um perfil.

--- Autoriza/Autoriza/Controllers/PerfilController.cs
+++ Autoriza/Autoriza/Controllers/PerfilController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using System.Web.Routing;
 using Autoriza.DAO;
 using Autoriza.Models;
+using Autoriza.Infra.NHibernate;
 
 namespace Autoriza.Controllers
 {
@@ -30,6 +31,7 @@ namespace Autoriza.Controllers
         }
 
         [HttpPost]
+        [Transaction]
         public ActionResult Novo(Perfil perfil)
         {
             try
@@ -46,5 +48,36 @@ namespace Autoriza.Controllers
             }
         }
 
+        public ActionResult Editar(int id)
+        {
+            try
+            {
+                Perfil perfil = PerfilDAO.Get(id);
+
+                return View(perfil);
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Detalhar", "Sistema", new {id = id});
+            }
+        }
+
+        [HttpPost]
+        [Transaction]
+        public ActionResult Editar(Perfil perfil)
+        {
+            try
+            {
+                perfil.Sistema = SistemaDAO.Get(perfil.Sistema.Id);
+
+                PerfilDAO.Update(perfil);
+
+                return RedirectToAction("Detalhar", "Sistema", new { id = perfil.Sistema.Id });
+            }
+            catch (Exception)
+            {
+                return View(perfil);
+            }
+        }
     }
 }
