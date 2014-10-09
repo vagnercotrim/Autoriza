5a5d674b03f8b8529ffdbb86e0dc39cb0d4257b1
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2014-10-09 15:08:35 -0400

    Criando o model Usuario e o mapeamento do NHibernate.

---
+++ Autoriza/Autoriza/Models/Mapping/UsuarioMap.cs
@@ -0,0 +1,20 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Autoriza.Models.Mapping
+{
+    public class UsuarioMap : ClassMap<Usuario>
+    {
+        public UsuarioMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Login).Not.Nullable().Unique();
+            Map(x => x.Senha).Not.Nullable();
+            Map(x => x.Email).Not.Nullable().Unique();
+            Map(x => x.Ativo).Not.Nullable();
+        }
+    }
+}
---
+++ Autoriza/Autoriza/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Autoriza.Models
+{
+    public class Usuario
+    {
+
+        public virtual int Id { get; set; }
+
+        public virtual String Login { get; set; }
+
+        public virtual String Senha { get; set; }
+
+        public virtual String Email { get; set; }
+
+        public virtual bool Ativo { get; set; }
+
+    }
+}
