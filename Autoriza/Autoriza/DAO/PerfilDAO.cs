723b77d0f4953d5cd4980166d3a95a135dfe973f
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2014-10-14 00:31:20 -0400

    Pesquisa um perfil por sistema e nome;

--- Autoriza/Autoriza/DAO/PerfilDAO.cs
+++ Autoriza/Autoriza/DAO/PerfilDAO.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using NHibernate.Criterion;
 
 namespace Autoriza.DAO
 {
@@ -11,10 +12,12 @@ namespace Autoriza.DAO
     {
 
         private readonly GenericDAO<Perfil> dao;
+        private readonly ISession session;
 
         public PerfilDAO(ISession session)
         {
             dao = new GenericDAO<Perfil>(session);
+            this.session = session;
         }
 
         public Perfil Get(int id)
@@ -33,5 +36,11 @@ namespace Autoriza.DAO
             dao.Update(perfil);
         }
 
+        public Perfil FindByNome(int idSistema, string nome)
+        {
+            ICriteria criteria = session.CreateCriteria<Perfil>().Add(Expression.Eq("Sistema.Id", idSistema)).Add(Expression.Eq("Nome", nome));
+
+            return criteria.UniqueResult<Perfil>();
+        }
     }
 }
