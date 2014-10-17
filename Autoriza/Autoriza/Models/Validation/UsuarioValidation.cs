b41575082d5b6d9e2da786b68b9523c82b79d365
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2014-10-17 00:24:37 -0400

    Validação do model usuario.

---
+++ Autoriza/Autoriza/Models/Validation/UsuarioValidation.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Autoriza.Models.Validation
+{
+    public class UsuarioValidation : AbstractValidator<Usuario>
+    {
+
+        public UsuarioValidation()
+        {
+            RuleFor(usuario => usuario.Login).NotEmpty();
+            RuleFor(usuario => usuario.Senha).NotEmpty();
+            RuleFor(usuario => usuario.Email).NotEmpty().EmailAddress();
+        }
+
+    }
+}
