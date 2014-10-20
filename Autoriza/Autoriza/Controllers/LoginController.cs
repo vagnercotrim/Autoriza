52c9a0b2d177acc0311dbe631b41eb8f23b36820
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2014-10-20 18:51:58 -0400

    Adicionando parametro urlRetorno para o Autoriza saber onde retornar quando a autenticação estiver concluída.

--- Autoriza/Autoriza/Controllers/LoginController.cs
+++ Autoriza/Autoriza/Controllers/LoginController.cs
@@ -8,11 +8,13 @@ namespace Autoriza.Controllers
 {
     public class LoginController : Controller
     {
- 
+
+
         [HttpPost]
-        public ActionResult Login(String identificacao, String acesso)
+        public ActionResult Login(String identificacao, String acesso, String urlRetorno)
         {
             return Json(new {Nome = "nome", Email = "email"}, JsonRequestBehavior.AllowGet);
         }
+
     }
 }
--- Sistema/Sistema/Controllers/HomeController.cs
+++ Sistema/Sistema/Controllers/HomeController.cs
@@ -14,14 +14,16 @@ namespace Sistema.Controllers
         {
             RestClient client = new RestClient(@"http://localhost:24657");
 
-            IRestRequest request = Post(@"login/login").AddParameter("identificacao", "autoriza_sso").AddParameter("acesso", "sksn48ndj");
+            IRestRequest request = Post(@"login/login").AddParameter("identificacao", "autoriza_sso")
+                                                       .AddParameter("acesso", "sksn48ndj")
+                                                       .AddParameter("urlRetorno", "http://localhost:40964/");
 
             var response = client.Execute<Usuario>(request);
             Usuario user = response.Data;
 
             return View();
         }
-        
+
         private IRestRequest Post(String recurso)
         {
             IRestRequest request = new RestRequest(recurso, Method.POST);
