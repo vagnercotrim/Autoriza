e91c78a2797e86c6fe53dac8239e2f5767ebe17d
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2014-10-20 19:06:37 -0400

    Excluindo arquivos do template.

--- Sistema/Sistema/Startup.cs
+++ Sistema/Sistema/Startup.cs
@@ -4,11 +4,10 @@ using Owin;
 [assembly: OwinStartupAttribute(typeof(Sistema.Startup))]
 namespace Sistema
 {
-    public partial class Startup
+    public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
         }
     }
 }
