f7090864939ebac1abf8fdd1d11942e9c901b03d
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2014-10-21 20:59:37 -0400

    A classe Autenticador vai validar o login e senha do usuário.

--- Autoriza/Autoriza/Controllers/LoginController.cs
+++ Autoriza/Autoriza/Controllers/LoginController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Web.Mvc;
 using Autoriza.DAO;
+using Autoriza.Domain;
 using Autoriza.Models;
 using Autoriza.ViewModels;
 
@@ -29,6 +30,8 @@ namespace Autoriza.Controllers
         public ActionResult Login(LoginView login)
         {
             login.Sistema = _sistemaDao.Get(login.Sistema.Id);
+
+            Autenticador aut = new Autenticador(login.Usuario);
 
             return View(login);
         }
---
+++ Autoriza/Autoriza/Domain/Autenticador.cs
@@ -0,0 +1,16 @@
+﻿using Autoriza.Models;
+
+namespace Autoriza.Domain
+{
+    public class Autenticador
+    {
+
+        private Usuario _usuario;
+
+        public Autenticador(Usuario usuario)
+        {
+            _usuario = usuario;
+        }
+
+    }
+}
