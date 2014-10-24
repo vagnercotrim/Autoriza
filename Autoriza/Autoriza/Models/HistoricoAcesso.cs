2a94d8e869e7229edb599884e101bb830696097a
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2014-10-23 23:53:49 -0400

    Definindo model para salvar todos os acessos feitos pelos usuários logados.

---
+++ Autoriza/Autoriza/Models/HistoricoAcesso.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Autoriza.Models
+{
+    public class HistoricoAcesso
+    {
+
+        public virtual int Id { get; set; }
+
+        public virtual DateTime Data { get; set; }
+
+        public virtual String Ip { get; set; }
+
+        public virtual String Navegador { get; set; }
+
+        public virtual Usuario Usuario { get; set; }
+
+        public virtual Sistema Sistema { get; set; }
+
+        public HistoricoAcesso(Usuario usuario, Sistema sistema, DateTime data, String ip, String navegador)
+        {
+            Usuario = usuario;
+            Sistema = sistema;
+            Data = data;
+            Ip = ip;
+            Navegador = navegador;
+        }
+
+    }
+}
---
+++ Autoriza/Autoriza/Models/Mapping/HistoricoAcessoMap.cs
@@ -0,0 +1,21 @@
+﻿using FluentNHibernate.Mapping;
+
+namespace Autoriza.Models.Mapping
+{
+    public class HistoricoAcessoMap : ClassMap<HistoricoAcesso>
+    {
+
+        public HistoricoAcessoMap()
+        {
+            Id(h => h.Id);
+
+            Map(h => h.Data);
+            Map(h => h.Ip);
+            Map(h => h.Navegador);
+
+            References(h => h.Usuario);
+            References(h => h.Sistema);
+        }
+
+    }
+}
