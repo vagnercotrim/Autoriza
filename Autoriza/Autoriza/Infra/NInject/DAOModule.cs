6b29b60c67dee4d40385da77e0f8c3e36a2db1d0
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2014-10-26 19:59:56 -0400

    Criando os DAOs em "requestscope"

--- Autoriza/Autoriza/Infra/NInject/DAOModule.cs
+++ Autoriza/Autoriza/Infra/NInject/DAOModule.cs
@@ -1,5 +1,6 @@
 ﻿using Autoriza.DAO;
 using Ninject.Modules;
+using Ninject.Web.Common;
 
 namespace Autoriza.Infra.NInject
 {
@@ -7,10 +8,10 @@ namespace Autoriza.Infra.NInject
     {
         public override void Load()
         {
-            Kernel.Bind<SistemaDAO>().To<SistemaDAO>();
-            Kernel.Bind<PermissaoDAO>().To<PermissaoDAO>();
-            Kernel.Bind<PerfilDAO>().To<PerfilDAO>();
-            Kernel.Bind<UsuarioDAO>().To<UsuarioDAO>();
+            Kernel.Bind<SistemaDAO>().To<SistemaDAO>().InRequestScope();
+            Kernel.Bind<PermissaoDAO>().To<PermissaoDAO>().InRequestScope();
+            Kernel.Bind<PerfilDAO>().To<PerfilDAO>().InRequestScope();
+            Kernel.Bind<UsuarioDAO>().To<UsuarioDAO>().InRequestScope();
         }
     }
 }
